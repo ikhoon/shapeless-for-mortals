72d770d446158a5755d3d92b1958aa62b8ea8185
Ikhoon Eom <ih.pert@gmail.com>
2017-08-11 00:40:39 +0900

    --wip-- [skip ci]

--- smbd/src/main/scala/StringyMapBigData.scala
+++ smbd/src/main/scala/StringyMapBigData.scala
@@ -42,12 +42,85 @@ package object impl {
   import api._
 
   // EXERCISE 1.1 goes here
-  // implicit def hNilBigDataFormat = ???
-  // implicit def hListBigDataFormat = ???
-  // implicit def cNilBigDataFormat = ???
-  // implicit def coproductBigDataFormat = ???
+   implicit def hNilBigDataFormat: BigDataFormat[HNil] =
+     new BigDataFormat[HNil] {
+       def label: String = "HNil"
+       def toProperties(t: HNil): StringyMap = new StringyMap
+       def fromProperties(m: StringyMap): BigResult[HNil] = Right(HNil)
+     }
 
-  implicit def familyBigDataFormat[T] = ???
+   implicit def hListBigDataFormat[K <: Symbol, V, L <: HList]
+    (implicit
+      key: Witness.Aux[K],
+      tv: Typeable[V],
+      F: BigDataFormat[L]
+    ): BigDataFormat[FieldType[K, V] :: L] =
+     new BigDataFormat[FieldType[K, V] :: L] {
+       def label = key.value.name + " :: " + F.label
+
+       def toProperties(t: FieldType[K, V] :: L): StringyMap = {
+         val map: StringyMap = F.toProperties(t.tail)
+         map.put(label, t.head)
+         map
+       }
+
+       def fromProperties(m: StringyMap): BigResult[FieldType[K, V] :: L] =  {
+         val v = tv.cast(m.get(key.value.name))
+         v match {
+           case Some(x) =>
+             F.fromProperties(m) match {
+               case Right(l) => Right(field[K](x) :: l)
+               case Left(err) => Left(err + s", ${key.value.name} does not exists.")
+             }
+           case _ => Left(s"${key.value.name} type ${tv.describe} does not matched.")
+         }
+       }
+     }
+
+   implicit def cNilBigDataFormat: BigDataFormat[CNil] =
+     new BigDataFormat[CNil] {
+       def label: String = "CNil"
+
+       def toProperties(t: CNil): StringyMap = new StringyMap
+
+       def fromProperties(m: StringyMap): BigResult[CNil] = Left("nothing")
+     }
+
+   implicit def coproductBigDataFormat[K <: Symbol, V, T <: Coproduct]
+    (implicit
+      key: Witness.Aux[K],
+      tv: Typeable[V],
+      F: BigDataFormat[T]
+    ): BigDataFormat[FieldType[K, V] :+: T] =
+     new BigDataFormat[FieldType[K, V] :+: T] {
+       def label = key.value.name + " :+: " + F.label
+       def toProperties(t: FieldType[K, V] :+: T): StringyMap = t match {
+         case Inl(l) =>
+           val map = new StringyMap
+           map.put(key.value.name, l)
+           map
+         case Inr(r) => F.toProperties(r)
+       }
+
+       def fromProperties(m: StringyMap): BigResult[FieldType[K, V] :+: T] = {
+         val v = tv.cast(m.get(key))
+         v match {
+           case Some(x) =>
+             Right(Inl(field[K](x)))
+           case _ => F.fromProperties(m) match {
+             case Right(r) => Right(Inr(r))
+             case Left(err) => Left(err)
+           }
+         }
+       }
+     }
+
+
+  implicit def familyBigDataFormat[T, R]
+    (implicit
+      gen: Generic.Aux[T, R],
+      F: BigDataFormat[R]
+    ): BigDataFormat[gen.Repr] = F
 }
 
 package impl {
